febfe433536d4cc21957e99e8632ca850f4a4c24
azimka <aszimka@gmail.com>
2019-01-13 13:34:47 +0300

    Initial commit

---
+++ src/main/java/com/azimka/multidatasource_spring/MultidatasourceSpringApplication.java
@@ -0,0 +1,14 @@
+package com.azimka.multidatasource_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultidatasourceSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultidatasourceSpringApplication.class, args);
+	}
+
+}
+
